a82da582fc14ade9d936ff43edfbef5b160b8a65
Arron Nguyen <arron.nguyen@mrjaffesclass.com>
2022-09-26 14:19:40 -0700

    dowhile and for loops

---
+++ ForLoops.java
@@ -0,0 +1,15 @@
+public class ForLoops
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(i + " is less than or equal to 15");
+        }
+        for (int j = 10; j >= 0; j += -2) {
+            System.out.println(j + " is less than or equal to 0");
+        }
+        for (int k = 10; k >= 0; k+= -3) {
+            System.out.println(k + " is less than or equal to 0");
+        }
+    }
+}
---
+++ HelloWorld2.java
@@ -0,0 +1,34 @@
+public class HelloWorld2
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+
+    public void run() {
+        int i = 10;
+        int k = 80;
+        do {
+            System.out.println(i + " is less than 15");
+            i += 1;
+        } while (i < 15);
+        do {
+            System.out.println( k + " is greater than 5");
+            k = k/2;
+        } while (k > 5);
+        do {
+            System.out.println(a*b);
+            a++;
+            b = b + 4;
+        } while (a < 10 && b <= 20);
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args)
+    {        
+        HelloWorld2 hw = new HelloWorld2();
+        hw.run();
+    }
+}
+
