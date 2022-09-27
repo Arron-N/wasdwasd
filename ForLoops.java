f86cc0318932ce4157b6701f09cf412b544061bc
Arron Nguyen <arron.nguyen@mrjaffesclass.com>
2022-09-27 14:14:26 -0700

    for edits

--- ForLoops.java
+++ ForLoops.java
@@ -5,11 +5,11 @@ public class ForLoops
         for (int i = 0; i <= 15; i++) {
             System.out.println(i + " is less than or equal to 15");
         }
-        for (int j = 10; j >= 0; j += -2) {
-            System.out.println(j + " is less than or equal to 0");
+        for (int j = 10; j != 0; j += -2) {
+            System.out.println(j + " is not equal to 0");
         }
-        for (int k = 10; k >= 0; k+= -3) {
-            System.out.println(k + " is less than or equal to 0");
+        for (int k = 10; k != 0; k += -3) {
+            System.out.println(k + " is not equal to 0");
         }
     }
 }
